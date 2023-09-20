0ee4b8e58019d379377d3c2f320e844ab923c24f
anmolko <anmol@canosoft.com.np>
2023-09-20 23:22:34 +0545

    reference agent login changes

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+        'agent' => [
+            'driver' => 'session',
+            'provider' => 'agents',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
@@ -69,6 +74,11 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+
+        'agents' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\ReferenceInformation::class,
         ],
 
         // 'users' => [
